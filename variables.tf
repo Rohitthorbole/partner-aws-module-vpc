08d9506f991547c42a8a57d3e87824eba0881581
Rohit Thorbole <rohitthorbole4@gmail.com>
2026-08-21 19:25:58 +0530

    config for vpc and subnet

--- variables.tf
+++ variables.tf
@@ -1,11 +1,30 @@
+//Common variables for the VPC module
 variable "region" {
     type = string
     default = "us-ease-1"
     description = "The AWS region where the VPC will be created."
 }
 
+//variables for the VPC
+
 variable "vpc_name" {
     type = string
     default = "my-vpc"
     description = "The name of the VPC."
+}
+
+variable "vpc_cidr" {
+  type = string
+  description = "The cidr of the vpc. Must be provided"
+}
+
+//variables for the subnets
+variable "subnet_name" {
+    type = string
+    description = "The name of the subnet."
+}
+
+variable "subnet_cidr" {
+    type = string
+    description = "The cidr of the subnet. Must be provided"
 }
